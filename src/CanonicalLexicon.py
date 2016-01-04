35e9d135c612d77354819f2d049a5ea26152fded
Benjamin Kosmehl <g.benjamin@gmx.de>
2016-01-04 12:38:27 +0100

    Hans

---
+++ src/CanonicalLexicon.py
@@ -0,0 +1,17 @@
+'''
+Created on 04.01.2016
+
+@author: Albert
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ src/MyNLTKTree.py
@@ -0,0 +1,18 @@
+'''
+Created on 04.01.2016
+
+@author: Albert
+'''
+from nltk import tree
+
+class MyClass(tree.Tree):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
